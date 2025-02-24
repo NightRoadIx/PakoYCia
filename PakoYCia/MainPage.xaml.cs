2c4b4890082186a9d14e12ff92488ef46afb6f1e
s_bio <surionelda@hotmail.com>
2025-02-24 16:53:56 -0600

    Adicionar arquivos de projeto.

---
+++ PakoYCia/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿namespace PakoYCia
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        // Get the color of button in mainpage
+        Color original;
+        
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            // Incrementar count
+            count++;
+
+            if (count == 1)
+            {
+                original = (Color)sender;
+                CounterBtn.Text = $"Me tocaste {count} vez";
+            }
+            else
+            { 
+                CounterBtn.Text = $"Me tocaste {count} veces";
+                if(count > 50)
+                    CounterBtn.BackgroundColor = Color.FromRgb(255, 0, 0);
+            }
+            
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private void OnResetClicked(object sender, EventArgs e)
+        {
+            count = 0;
+            CounterBtn.Text = "Tócame de nuevo, estoy list@";
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+        
+    }
+
+}
---
+++ PakoYCia/MauiProgram.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace PakoYCia
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+// Para la creación de un programa
+// Modo de prueba DEBUG
+// Modo de producción
+#if DEBUG
+            // Log o archivo de captura de errores / excepciones
+    		builder.Logging.AddDebug();
+#endif
+            return builder.Build();
+        }
+    }
+}
